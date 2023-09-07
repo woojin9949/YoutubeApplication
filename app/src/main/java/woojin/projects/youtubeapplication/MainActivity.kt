1e16f89bbeaeac8da81473708e1f83619dc76c76
woojin <woojin9949@gmail.com>
2023-09-07 20:27:11 +0900

    update

--- app/src/main/java/woojin/projects/youtubeapplication/MainActivity.kt
+++ app/src/main/java/woojin/projects/youtubeapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package woojin.projects.youtubeapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,8 +19,9 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private lateinit var videoAdapter: VideoAdapter
+    private lateinit var videoList: List<Youtuber>
 
-    //private lateinit var videoAdapter: VideoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -27,50 +29,49 @@ class MainActivity : AppCompatActivity() {
         val retrofitService = setRetrofitService()
         getVideo(retrofitService)
 
-        //videoAdapter = VideoAdapter()
-
-//        binding.videoListRecyclerView.apply {
-//            layoutManager = LinearLayoutManager(context)
-//            adapter = videoAdapter
-//        }
+        videoAdapter = VideoAdapter(context = applicationContext)
+        binding.videoListRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = videoAdapter
+        }
+        binding.playerRecyclerView
 
     }
 
-
-}
-
-private fun getVideo(retrofitService: RetrofitService) {
-    retrofitService.getVideos().enqueue(object : Callback<Videos> {
-        override fun onResponse(call: Call<Videos>, response: Response<Videos>) {
-            if (response.isSuccessful) {
-                //Pexels에서 받아온 영상 리스트 대입
-                val list = response.body()?.videos?.map { videoDatas ->
-                    Youtuber(
-                        videoDatas.videoFiles.first().link,
-                        videoDatas.user.name,
-                        videoDatas.image
-                    )
+    private fun getVideo(retrofitService: RetrofitService) {
+        retrofitService.getVideos().enqueue(object : Callback<Videos> {
+            override fun onResponse(call: Call<Videos>, response: Response<Videos>) {
+                if (response.isSuccessful) {
+                    //Pexels에서 받아온 영상 리스트 대입
+                    videoList = response.body()?.videos?.map { videoDatas ->
+                        //가져온 정보 link와 사용자 이름 추출 하여 데이터 클래스 대입
+                        Youtuber(
+                            videoDatas.videoFiles.first().link,
+                            videoDatas.user.name,
+                            videoDatas.image
+                        )
+                    } ?: return
+                    videoAdapter.submitList(videoList)
+                } else {
+                    Log.e("MainActivity", "오류")
                 }
-                //가져온 정보 link와 사용자 이름 추출 하여 데이터 클래스 대입
-                Log.e("testt", list.toString())
-            } else {
-                Log.e("MainActivity", "오류")
             }
-        }
 
-        override fun onFailure(call: Call<Videos>, t: Throwable) {
-            Log.e("MainActivity", "${t.printStackTrace()}")
-        }
-    })
+            override fun onFailure(call: Call<Videos>, t: Throwable) {
+                Log.e("MainActivity", "${t.printStackTrace()}")
+            }
+        })
+    }
+
+    private fun setRetrofitService(): RetrofitService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.pexels.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return retrofit.create(RetrofitService::class.java)
+    }
 
 }
 
-private fun setRetrofitService(): RetrofitService {
-    val retrofit = Retrofit.Builder()
-        .baseUrl("https://api.pexels.com/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
 
-    val retrofitService = retrofit.create(RetrofitService::class.java)
-    return retrofitService
-}
--- app/src/main/java/woojin/projects/youtubeapplication/VideoAdapter.kt
+++ app/src/main/java/woojin/projects/youtubeapplication/VideoAdapter.kt
@@ -1,19 +1,31 @@
 package woojin.projects.youtubeapplication
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil.ItemCallback
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import woojin.projects.youtubeapplication.databinding.ItemVideoBinding
 import woojin.projects.youtubeapplication.model.Youtuber
 
-class VideoAdapter : ListAdapter<Youtuber, VideoAdapter.ViewHolder>(diffUtil) {
+class VideoAdapter(private val context: Context) :
+    ListAdapter<Youtuber, VideoAdapter.ViewHolder>(diffUtil) {
 
     inner class ViewHolder(private val binding: ItemVideoBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(item: Youtuber) {
+            Glide.with(binding.videoThumbnailImageView)
+                .load(item.image)
+                .into(binding.videoThumbnailImageView)
 
+            Glide.with(binding.channelLogoImageView)
+                .load(R.drawable.baseline_person_24)
+                .into(binding.channelLogoImageView)
+
+            binding.titleTextView.text = "제목"
+            binding.nameTextView.text = context.getString(R.string.video_info, item.name)
         }
     }
 
